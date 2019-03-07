95e1ceb173ab07fdc8a71e9ffbc9f6066155a85c
cougar91 <alpha.z91@mail.ru>
2019-03-07 14:10:09 +0600

    Добавьте файлы проекта.

---
+++ Card_Game/PlayingCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Game
+{
+    public class PlayingCard
+    {
+        public Suits Suit { get; set; }
+        public Types Type { get; set; }
+    }
+}
---
+++ Card_Game/Types.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Game
+{
+    public enum Types
+    {
+        Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+    }
+}
